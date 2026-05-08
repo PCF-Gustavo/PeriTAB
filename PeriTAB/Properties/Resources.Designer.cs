3978a71f97edfd5999ce9fd125234e5ea080dbd0
gustavo.gvs <gustavo.gvs@MASETEC31224>
2026-05-08 10:11:46 -0300

    excluir lupa

--- PeriTAB/Properties/Resources.Designer.cs
+++ PeriTAB/Properties/Resources.Designer.cs
@@ -201,16 +201,6 @@ namespace PeriTAB.Properties {
         }
         
         /// <summary>
-        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap lupa {
-            get {
-                object obj = ResourceManager.GetObject("lupa", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Consulta um recurso localizado do tipo System.Byte[].
         /// </summary>
         internal static byte[] Normal {
